192c1c58d35302850630867f010690a72e6c7a07
Saša Barišić <cartman300@net.hr>
2014-10-10 21:08:59 +0200

    Bare bones

---
+++ DDVG/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using SFML.Graphics;
+using SFML.Window;
+
+namespace DDVG {
+	class Program {
+		static void Main(string[] args) {
+			Renderer R = new Renderer(800, 600);
+			Stopwatch SWatch = new Stopwatch();
+			SWatch.Start();
+
+			while (R.IsOpen()) {
+				R.DispatchEvents();
+				R.Update((float)SWatch.ElapsedMilliseconds / 1000);
+				R.Render();
+				SWatch.Restart();
+			}
+		}
+	}
+}
---
+++ DDVG/Renderer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SFML.Graphics;
+using SFML.Window;
+
+namespace DDVG {
+	class Renderer : RenderWindow {
+		List<Sprite> Sprites;
+
+		public Renderer(int W, int H)
+			: base(new VideoMode((uint)W, (uint)H), "The Game", Styles.Close) {
+			Closed += (S, E) => Close();
+
+			Sprites = new List<Sprite>();
+			Random Rand = new Random();
+
+			for (int i = 0; i < 4; i++) {
+				Sprite S = new Sprite(new Texture("data/textures/car.png"));
+				S.Position = new Vector2f((float)W / 4 * i, (float)H / 5);
+				S.Scale = new Vector2f(0.65f, 0.65f);
+				S.Color = new Color((byte)Rand.Next(0, 255), (byte)Rand.Next(0, 255), (byte)Rand.Next(0, 255));
+
+				Sprites.Add(S);
+			}
+		}
+
+		public void Update(float T) {
+
+		}
+
+		public void Render() {
+			Clear(Color.Black);
+			for (int i = 0; i < Sprites.Count; i++)
+				Draw(Sprites[i]);
+
+			Display();
+		}
+	}
+}
